66c4d742821652375349da3e87214f7d0adddea5
Tom Ruscelli <tom.ruscelli@ynov.com>
2018-05-31 21:20:15 +0200

    Branche à delete, nouveau systeme de notation

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,8 @@
 class EpisodesController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
   def show
     @episode = Episode.find(params[:id])
     @notes = Note.where("episode_id = ?", params[:id])
+    @new_note = Note.new
   end
 end
