271753f68c03167b0a465f23172353ac4c00e5bb
Tom Ruscelli <tom.ruscelli@ynov.com>
2018-06-03 00:13:03 +0200

    test average note on season page

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,7 +1,7 @@
 class EpisodesController < ApplicationController
   def show
       @episode = Episode.find(params[:id])
-      @notes = Note.where("episode_id = ?", params[:id]) 
+      @notes = Note.where("episode_id = ?", params[:id])
       @new_note = Note.new
   end
 end
