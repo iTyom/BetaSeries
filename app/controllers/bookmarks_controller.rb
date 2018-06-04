10765b39413aff835968d59cf9dc4f85fa33666a
Tom Ruscelli <tom.ruscelli@ynov.com>
2018-06-04 15:27:27 +0200

    bookmark btn ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,11 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
-    def create
+    def show
+      @bookmarks = Bookmark.all
+    end
+
+    def new
       @serie = Serie.find(params[:series_id])
       @new_bookmark = Bookmark.new(bookmark_params)
       @new_bookmark.serie = @serie
@@ -14,6 +18,13 @@ class BookmarksController < ApplicationController
       else
         render "series/show"
       end
+    end
+
+    def destroy
+      @serie = Serie.find(params[:series_id])
+      @delete_bookmark = Bookmark.where("serie_id = ? AND user_id = ?", @serie, current_user).delete_all
+      flash[:notice] = "Votre série a été supprimé."
+      redirect_to series_path(@serie)
     end
 
     private
